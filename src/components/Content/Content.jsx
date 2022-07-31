04ccd66131981cc6c2acfca4cfd27e0e898141ca
Tuan Tran <langthamht047@gmail.com>
2022-07-31 03:49:40 -0700

    handle animated for content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,38 +1,45 @@
 import { FaUser } from "react-icons/fa";
+import { animated, useSpring } from "@react-spring/web";
 import { BsFillCreditCardFill } from "react-icons/bs";
-import CheckIconPoint from "../../assets/mobile/checkpoint.svg"
+import CheckIconPoint from "../../assets/mobile/checkpoint.svg";
 const Content = () => {
+  const customer = useSpring({ customers: 10073, from: { customers: 0 } });
+  const card = useSpring({ cards: 10073, from: { cards: 0 } });
   return (
     <section className="">
       <div className="ml-[70px] mr-[54px] flex flex-col items-center  text-bold  bg-gradient-to-b shadow-md from-white/40 rounded-[24px] to-transparent">
         <div className="mt-16 gap-8 flex items-start">
           <FaUser size={"22px"} className="mt-[12px]" />
-          <p className="text-26 ">
-            1007
+          <div className="text-26 ">
+            <animated.div>
+              {customer.customers.to((val) => Math.floor(val))}
+            </animated.div>
             <p className="text-13 font-semibold mb-[36px]">Customers</p>
-          </p>
+          </div>
         </div>
         <div className="gap-8 flex items-start mb-16">
           <BsFillCreditCardFill size={"22px"} className="mt-[12px]" />
-          <p className="text-26">
-            1007
+          <div className="text-26">
+            <animated.div>
+              {card.cards.to((val) => Math.floor(val))}
+            </animated.div>
             <p className="text-13 font-semibold">Card Issued</p>
-          </p>
+          </div>
         </div>
       </div>
       <div className="text-13 flex flex-col w-[100%] font-semibold mt-[90px]">
-          <p className="check-content">
-             <img src={CheckIconPoint} alt="icon check" />
-             <p>Card reports send to your phone every weeks</p>
-          </p>
-          <p className="check-content">
-             <img src={CheckIconPoint} alt="icon check" />
-             <p>No external fees</p>
-          </p>
-          <p className="check-content">
-             <img src={CheckIconPoint} alt="icon check" />
-             <p>Set spanding limits and restrictions</p>
-          </p>
+        <div className="check-content">
+          <img src={CheckIconPoint} alt="icon check" />
+          <p>Card reports send to your phone every weeks</p>
+        </div>
+        <div className="check-content">
+          <img src={CheckIconPoint} alt="icon check" />
+          <p>No external fees</p>
+        </div>
+        <div className="check-content">
+          <img src={CheckIconPoint} alt="icon check" />
+          <p>Set spanding limits and restrictions</p>
+        </div>
       </div>
     </section>
   );
